2fd8e21c36f484465795f3fe5ae7cffdd2e229bb
467471641@qq.com <467471641@qq.com>
2016-10-09 09:52:11 +0800

    第一次提交

---
+++ app/src/main/java/com/cp/xiaoshutouutil/MainActivity.java
@@ -0,0 +1,18 @@
+package com.cp.xiaoshutouutil;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.cp.xst.mylibrary.MTestUitl;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+   
+    }
+}
---
+++ xstutil/src/main/java/com/cp/xst/mylibrary/MTestUitl.java
@@ -0,0 +1,14 @@
+package com.cp.xst.mylibrary;
+
+/**
+ * Created by Jerry on 2016/10/9.
+ */
+public class MTestUitl {
+
+    public static void testCp()
+    {
+        System.out.print(" 林楚萍,,,");
+    }
+
+
+}
