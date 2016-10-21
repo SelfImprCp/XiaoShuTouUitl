3f302276b98e7956285d3db84a99bceec2fe507b
467471641@qq.com <467471641@qq.com>
2016-10-21 10:14:17 +0800

    添加隐藏身份证号

--- mylibrary/src/main/java/com/cp/mylibrary/utils/IDCardUitl.java
+++ mylibrary/src/main/java/com/cp/mylibrary/utils/IDCardUitl.java
@@ -28,82 +28,81 @@ public class IDCardUitl {
     /**
      * 功能：身份证的有效验证
      *
-     * @param IDStr
-     *            身份证号
+     * @param IDStr 身份证号
      * @return 有效：返回"" 无效：返回String信息
      * @throws ParseException
      */
     @SuppressWarnings("unchecked")
     public static String IDCardValidate(String IDStr) throws ParseException {
         String errorInfo = "";// 记录错误信息
-        String[] ValCodeArr = { "1", "0", "x", "9", "8", "7", "6", "5", "4",
-                "3", "2" };
-            String[] Wi = { "7", "9", "10", "5", "8", "4", "2", "1", "6", "3", "7",
-                    "9", "10", "5", "8", "4", "2" };
-            String Ai = "";
-            // ================ 号码的长度 15位或18位 ================
-            if (IDStr.length() != 15 && IDStr.length() != 18) {
-                errorInfo = "身份证号码长度应该为15位或18位。";
-                return errorInfo;
-            }
-            // =======================(end)========================
+        String[] ValCodeArr = {"1", "0", "x", "9", "8", "7", "6", "5", "4",
+                "3", "2"};
+        String[] Wi = {"7", "9", "10", "5", "8", "4", "2", "1", "6", "3", "7",
+                "9", "10", "5", "8", "4", "2"};
+        String Ai = "";
+        // ================ 号码的长度 15位或18位 ================
+        if (IDStr.length() != 15 && IDStr.length() != 18) {
+            errorInfo = "身份证号码长度应该为15位或18位。";
+            return errorInfo;
+        }
+        // =======================(end)========================
 
-            // ================ 数字 除最后以为都为数字 ================
-            if (IDStr.length() == 18) {
-                Ai = IDStr.substring(0, 17);
-            } else if (IDStr.length() == 15) {
-                Ai = IDStr.substring(0, 6) + "19" + IDStr.substring(6, 15);
-            }
-            if (isNumeric(Ai) == false) {
-                errorInfo = "身份证15位号码都应为数字 ; 18位号码除最后一位外，都应为数字。";
-                return errorInfo;
-            }
-            // =======================(end)========================
+        // ================ 数字 除最后以为都为数字 ================
+        if (IDStr.length() == 18) {
+            Ai = IDStr.substring(0, 17);
+        } else if (IDStr.length() == 15) {
+            Ai = IDStr.substring(0, 6) + "19" + IDStr.substring(6, 15);
+        }
+        if (isNumeric(Ai) == false) {
+            errorInfo = "身份证15位号码都应为数字 ; 18位号码除最后一位外，都应为数字。";
+            return errorInfo;
+        }
+        // =======================(end)========================
 
-            // ================ 出生年月是否有效 ================
-            String strYear = Ai.substring(6, 10);// 年份
-            String strMonth = Ai.substring(10, 12);// 月份
-            String strDay = Ai.substring(12, 14);// 月份
-            if (isDate(strYear + "-" + strMonth + "-" + strDay) == false) {
-                errorInfo = "身份证生日无效。";
+        // ================ 出生年月是否有效 ================
+        String strYear = Ai.substring(6, 10);// 年份
+        String strMonth = Ai.substring(10, 12);// 月份
+        String strDay = Ai.substring(12, 14);// 月份
+        if (isDate(strYear + "-" + strMonth + "-" + strDay) == false) {
+            errorInfo = "身份证生日无效。";
+            return errorInfo;
+        }
+        GregorianCalendar gc = new GregorianCalendar();
+        SimpleDateFormat s = new SimpleDateFormat("yyyy-MM-dd");
+        try {
+            if ((gc.get(Calendar.YEAR) - Integer.parseInt(strYear)) > 150
+                    || (gc.getTime().getTime() - s.parse(
+                    strYear + "-" + strMonth + "-" + strDay).getTime()) < 0) {
+                errorInfo = "身份证生日不在有效范围。";
                 return errorInfo;
             }
-            GregorianCalendar gc = new GregorianCalendar();
-            SimpleDateFormat s = new SimpleDateFormat("yyyy-MM-dd");
-            try {
-                if ((gc.get(Calendar.YEAR) - Integer.parseInt(strYear)) > 150
-                        || (gc.getTime().getTime() - s.parse(
-                        strYear + "-" + strMonth + "-" + strDay).getTime()) < 0) {
-                    errorInfo = "身份证生日不在有效范围。";
-                    return errorInfo;
-                }
-            } catch (NumberFormatException e) {
-                e.printStackTrace();
-            } catch (ParseException e) {
-                e.printStackTrace();
-            }
-            if (Integer.parseInt(strMonth) > 12 || Integer.parseInt(strMonth) == 0) {
-                errorInfo = "身份证月份无效";
-                return errorInfo;
-            }
-            if (Integer.parseInt(strDay) > 31 || Integer.parseInt(strDay) == 0) {
-                errorInfo = "身份证日期无效";
-                return errorInfo;
-            }
-            // =====================(end)=====================
+        } catch (NumberFormatException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        if (Integer.parseInt(strMonth) > 12 || Integer.parseInt(strMonth) == 0) {
+            errorInfo = "身份证月份无效";
+            return errorInfo;
+        }
+        if (Integer.parseInt(strDay) > 31 || Integer.parseInt(strDay) == 0) {
+            errorInfo = "身份证日期无效";
+            return errorInfo;
+        }
+        // =====================(end)=====================
 
-            // ================ 地区码时候有效 ================
-            Hashtable h = GetAreaCode();
-            if (h.get(Ai.substring(0, 2)) == null) {
-                errorInfo = "身份证地区编码错误。";
-                return errorInfo;
-            }
-            // ==============================================
+        // ================ 地区码时候有效 ================
+        Hashtable h = GetAreaCode();
+        if (h.get(Ai.substring(0, 2)) == null) {
+            errorInfo = "身份证地区编码错误。";
+            return errorInfo;
+        }
+        // ==============================================
 
-            // ================ 判断最后一位的值 ================
-            int TotalmulAiWi = 0;
-            for (int i = 0; i < 17; i++) {
-                TotalmulAiWi = TotalmulAiWi
+        // ================ 判断最后一位的值 ================
+        int TotalmulAiWi = 0;
+        for (int i = 0; i < 17; i++) {
+            TotalmulAiWi = TotalmulAiWi
                     + Integer.parseInt(String.valueOf(Ai.charAt(i)))
                     * Integer.parseInt(Wi[i]);
         }
@@ -217,9 +216,19 @@ public class IDCardUitl {
     }
     /*********************************** 身份证验证结束 ****************************************/
 
+    /**
+     * 隐藏部分身份证号
+     *
+     * @param
+     * @throws ParseException
+     */
 
+    public static String hideId(String ID) throws ParseException {
 
+        String regex = "(\\w{6})(\\w+)(\\w{6})";
+        return ID.replaceAll(regex, "$1****$3");
 
+    }
 
 
 }
