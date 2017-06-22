719340b296c8d68416c053c905960c52b8c32710
467471641@qq.com <467471641@qq.com>
2017-06-22 15:51:27 +0800

    添加选择省市区

--- mylibrary/src/main/java/com/cp/mylibrary/custom/ProgressWebView.java
+++ mylibrary/src/main/java/com/cp/mylibrary/custom/ProgressWebView.java
@@ -18,18 +18,24 @@ import com.cp.mylibrary.R;
  *仿微信中加载网页时带线行进度条的WebView的实现
  */
 public class ProgressWebView extends WebView {
-    private ProgressBar progressbar;
+    //private ProgressBar progressbar;
 
     public ProgressWebView(Context context, AttributeSet attrs) {
         super(context, attrs);
-        progressbar = new ProgressBar(context, null,
-                android.R.attr.progressBarStyleHorizontal);
-        progressbar.setLayoutParams(new LayoutParams(LayoutParams.FILL_PARENT,
-                10, 0, 0));
 
-        Drawable drawable = context.getResources().getDrawable(R.drawable.progress_bar_states);
-        progressbar.setProgressDrawable(drawable);
-        addView(progressbar);
+//
+//        progressbar = new ProgressBar(context, null,
+//                android.R.attr.progressBarStyleHorizontal);
+//        progressbar.setLayoutParams(new LayoutParams(LayoutParams.FILL_PARENT,
+//                10, 0, 0));
+//
+//        Drawable drawable = context.getResources().getDrawable(R.drawable.progress_bar_states);
+//        progressbar.setProgressDrawable(drawable);
+//        addView(progressbar);
+//
+//
+
+
          setWebViewClient(new MyWebViewClient(){});
         setWebChromeClient(new WebChromeClient());
 
@@ -90,10 +96,10 @@ public class ProgressWebView extends WebView {
 
     @Override
     protected void onScrollChanged(int l, int t, int oldl, int oldt) {
-        LayoutParams lp = (LayoutParams) progressbar.getLayoutParams();
-        lp.x = l;
-        lp.y = t;
-        progressbar.setLayoutParams(lp);
+//        LayoutParams lp = (LayoutParams) progressbar.getLayoutParams();
+//        lp.x = l;
+//        lp.y = t;
+//        progressbar.setLayoutParams(lp);
         super.onScrollChanged(l, t, oldl, oldt);
     }
 
